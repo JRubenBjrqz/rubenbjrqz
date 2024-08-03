82f51b8ec5921eeb11c2c693f57bcf5b8a6c4c37
Rubén Bojórquez <159498245+jrubenbjrqzo@users.noreply.github.com>
2024-08-02 20:15:17 -0700

    Responsive layout

--- app/page.tsx
+++ app/page.tsx
@@ -7,23 +7,23 @@ export default function Home() {
   return (
     <>
       <Container>
-        <div className="grid grid-cols-10 grid-rows-6 gap-7 p-5 min-h-[52rem]">
-          <div className="relative col-span-4 row-span-4 border border-red-500">
+        <div className="grid grid-cols-10 grid-rows-6 gap-7 p-5 lg:min-h-[52rem]">
+          <div className="relative col-span-10 h-44 border border-red-500 md:h-56 md:col-span-5 lg:h-auto lg:col-span-4 lg:row-span-4">
             {/* About */}
           </div>
-          <div className="relative col-span-6 row-span-2 border border-blue-500">
+          <div className="relative col-span-10 border border-blue-500 md:col-span-5 lg:col-span-6 lg:row-span-2">
             {/* Work Experience */}
           </div>
-          <div className="relative col-span-3 row-span-2 border border-orange-500">
+          <div className="relative col-span-10 border border-orange-500 md:col-span-5 lg:col-span-3 lg:row-span-2">
             {/* Stack */}
           </div>
-          <div className="relative col-span-3 row-span-2 border border-green-500">
+          <div className="relative col-span-10 border border-green-500 md:col-span-5 lg:col-span-3 lg:row-span-2">
             {/* Contact */}
           </div>
-          <div className="relative col-span-7 row-span-2 border border-pink-500">
+          <div className="relative col-span-10 border border-pink-500 md:col-span-5 lg:col-span-7 lg:row-span-2">
             {/* Projects */}
           </div>
-          <div className="relative col-span-3 row-span-2 border border-yellow-500">
+          <div className="relative col-span-10 border border-yellow-500 md:col-span-5 lg:col-span-3 lg:row-span-2">
             {/* Learning */}
           </div>
         </div>
