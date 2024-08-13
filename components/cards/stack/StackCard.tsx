703fab4f4e3840c2180a26cf6e0406544bfa682e
Rubén Bojórquez <159498245+jrubenbjrqzo@users.noreply.github.com>
2024-08-12 19:26:19 -0700

    Orbiting circles component

--- components/cards/stack/StackCard.tsx
+++ components/cards/stack/StackCard.tsx
@@ -1,4 +1,5 @@
 import { Card } from '@/components/cards/Card'
+import { OrbitingCirclesDemo } from './StackOrbitingCircles'
 import { StackProps } from '@/interfaces'
 import { LiaToolsSolid } from 'react-icons/lia'
 
@@ -6,6 +7,7 @@ export function Stack({ className = '', ...props }: StackProps) {
     return (
         <Card className={className}>
             <Card.Pill icon={LiaToolsSolid}>Stack</Card.Pill>
+            <OrbitingCirclesDemo/>
         </Card>
     )
 }
