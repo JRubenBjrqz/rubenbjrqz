4e390e7c43a052547c4f7cc17f8040fdc8f740e6
Rubén Bojórquez <159498245+jrubenbjrqzo@users.noreply.github.com>
2024-08-12 19:26:08 -0700

    Added Orbiting circles from magic ui

---
+++ interfaces/OrbitingCircles.ts
@@ -0,0 +1,10 @@
+export interface OrbitingCirclesProps {
+    className?: string
+    children?: React.ReactNode
+    reverse?: boolean
+    duration?: number
+    delay?: number
+    radius?: number
+    path?: boolean
+}
+  
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,13 +8,24 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      animation: {
+        orbit: "orbit calc(var(--duration)*1s) linear infinite",
+      },
+      keyframes: {
+        orbit: {
+          "0%": {
+            transform:
+              "rotate(0deg) translateY(calc(var(--radius) * 1px)) rotate(0deg)",
+          },
+          "100%": {
+            transform:
+              "rotate(360deg) translateY(calc(var(--radius) * 1px)) rotate(-360deg)",
+          },
+        },
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
+
+export default config
