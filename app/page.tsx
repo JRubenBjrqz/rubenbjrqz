d5e000ac6e6364e9a03d4399ea7ed862292f2d9e
Rubén Bojórquez <159498245+jrubenbjrqzo@users.noreply.github.com>
2024-08-18 00:23:55 -0700

    shadow for bento box

--- app/page.tsx
+++ app/page.tsx
@@ -11,24 +11,12 @@ export default function Home() {
     <>
       <Container>
         <div className="grid grid-cols-10 grid-rows-6 gap-7 p-5 lg:min-h-[52rem]">
-          <About className="relative col-span-10 h-44 md:h-56 md:col-span-5 lg:h-auto lg:col-span-4 lg:row-span-4">
-            {/* About */}
-          </About>
-          <Resume className="relative col-span-10 md:col-span-5 lg:col-span-6 lg:row-span-2">
-            {/* Work Experience */}
-          </Resume>
-          <Stack className="relative col-span-10 md:col-span-5 lg:col-span-3 lg:row-span-2">
-            {/* Stack */}
-          </Stack>
-          <Contact className="relative col-span-10 md:col-span-5 lg:col-span-3 lg:row-span-2">
-            {/* Contact */}
-          </Contact>
-          <Projects className="relative col-span-10 md:col-span-5 lg:col-span-7 lg:row-span-2">
-            {/* Projects */}
-          </Projects>
-          <Learning className="relative col-span-10 md:col-span-5 lg:col-span-3 lg:row-span-2">
-            {/* Learning */}
-          </Learning>
+          <About className="relative col-span-10 h-44 md:h-56 md:col-span-5 lg:h-auto lg:col-span-4 lg:row-span-4" />
+          <Resume className="relative col-span-10 md:col-span-5 lg:col-span-6 lg:row-span-2" />
+          <Stack className="relative col-span-10 md:col-span-5 lg:col-span-3 lg:row-span-2" />
+          <Contact className="relative col-span-10 md:col-span-5 lg:col-span-3 lg:row-span-2" />
+          <Projects className="relative col-span-10 md:col-span-5 lg:col-span-7 lg:row-span-2" />
+          <Learning className="relative col-span-10 md:col-span-5 lg:col-span-3 lg:row-span-2" />
         </div>
       </Container>
     </>
